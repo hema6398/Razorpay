5efe23b95dbfa1205480b59554a4c4bba285c5e1
Hema <hemabaghel8650@gmail.com>
2025-07-30 02:57:44 -1200

    razorpay_clone

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["mullish","sans-serif"],
+      },
+    },
+    colors: {
+      deepblue: "#02042a",
+      lightblue: "#2b84ea",
+      lightblue300: "#4b94ed",
+      lightblue500: "#8b72e7",
+      greenlight: "#61cea4",
+      graytext: "#818597",
+      lightgray: "#e2e2e2",
+      grayblue: "#344a6c",
+      deepbluelead: "#162f56",
+      gray2: "#525a76",
+      white: "#fff",
+      slate:"#708090",
+      bluegradient:"#add8e6",
+      darkblue:"#1a73f8",
+      blue600:"#3182ce",
+      blue400:"#63b3ed",
+    },
+  },
+  plugins: [],
+}
+
